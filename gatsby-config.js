093dafa47abfa6786d8c4bb227dc31389e681cd6
drimpact <drimpact@users.noreply.github.com>
2020-04-07 21:10:09 +0100

    adding sitemap.xml and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Soft outcomes`,
+    description: `A catalogue of questionnaires suited to capturing soft outcomes`,
+    author: `impactasaurus`,
+    siteUrl: "https://softoutcomes.org"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -45,5 +46,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    'gatsby-plugin-robots-txt',
+    `gatsby-plugin-sitemap`
   ],
 }
